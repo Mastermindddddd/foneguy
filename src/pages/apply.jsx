4ea7b3f303f39c59f6671e73b2f7ebc32c5cdfc5
Mastermindddddd <malusicephas17@gmail.com>
2025-07-06 02:27:25 +0200

    ...

--- src/pages/apply.jsx
+++ src/pages/apply.jsx
@@ -6,6 +6,12 @@ const Apply = () => {
   return (
     <div>
       <div className="w-full h-14 bg-blue-600"></div>
+      <div className="w-full bg-gradient-to-r bg-cyan-700 py-6 text-center text-white mb-40">
+        <h2 className="text-xl md:text-3xl font-medium italic">
+          Start your application now & <br />
+          Join <span className="font-bold not-italic">FoneGuy</span> Today
+        </h2>
+      </div>
       <ApplicationForm />
       <section id="communication-preference" className="pt-18">
         <CommunicationPreferences />
