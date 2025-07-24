1c90b32f20882b8c04a29d59409da8964cb364d1
Mastermindddddd <malusicephas17@gmail.com>
2025-07-24 22:21:57 +0200

    ...

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/applications', applicationRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
